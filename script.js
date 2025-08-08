979b09a106398e722e44472de1cebe24ef418d54
Devanshuu18 <b240741@skit.ac.in>
2025-08-08 21:31:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMore() {
+  const surpriseText = document.getElementById("surprise-text");
+  surpriseText.innerText = "Always be the same energetic and cool 🥰";
+  surpriseText.style.display = "block";
+}
